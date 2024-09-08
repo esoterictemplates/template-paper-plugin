b1d296e209019383d18fbac79b04fd90daa15a1c
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-09-08 21:21:33 +0100

    Rename directory to 'resource-pack'

--- src/main/java/net/slqmy/template_paper_plugin/custom_entity/CustomEntity.java
+++ src/main/java/net/slqmy/template_paper_plugin/custom_entity/CustomEntity.java
@@ -1,5 +1,5 @@
 package net.slqmy.template_paper_plugin.custom_entity;
 
 public enum CustomEntity {
-  
+  EXPLOSIVE_SQUID;
 }
--- src/main/java/net/slqmy/template_paper_plugin/custom_item/CustomItem.java
+++ src/main/java/net/slqmy/template_paper_plugin/custom_item/CustomItem.java
@@ -1,5 +1,5 @@
 package net.slqmy.template_paper_plugin.custom_item;
 
 public enum CustomItem {
-  
+  DUMMY_CUSTOM_ITEM;
 }
