cc5ac435601120b8d51d00762137a5970b47a948
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-09-08 23:02:20 +0100

    Add documentation for `projectName` variable

--- scripts/rename-project.ts
+++ scripts/rename-project.ts
@@ -1,6 +1,15 @@
 const commandLineArguments = process.argv.slice(2);
 
-const projectName = commandLineArguments[0];
+/**
+ * The new name of the project, as provided by the person running the command.
+ * This name is in [kebab-case](https://developer.mozilla.org/en-US/docs/Glossary/Kebab_case) format.
+ * 
+ * The name is provided by running this command:
+ * `npm run rename-project -- new-project-name`
+ * 
+ * Where `new-project-name` is replaced with the desired plugin name, in [kebab-case](https://developer.mozilla.org/en-US/docs/Glossary/Kebab_case) format. 
+**/
+const projectName = commandLineArguments[0]; // npm run rename-project -- new-project-name
 
 if (!projectName) {
     console.error("A project name must be specified");
