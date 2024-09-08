72fac33b022a3c1b8454379cac84c2d268721bee
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-09-08 23:00:57 +0100

    Add more info about kebab-case

--- scripts/rename-project.ts
+++ scripts/rename-project.ts
@@ -7,5 +7,6 @@ if (!projectName) {
     console.error("Please use the following command:");
     console.error("npm run rename-project -- new-project-name");
     console.error("Where 'new-project-name' is replaced with your desired plugin name, in kebab-case format.");
+    console.error("Learn about kebab-case: https://developer.mozilla.org/en-US/docs/Glossary/Kebab_case");
     process.exit(1);
 }
