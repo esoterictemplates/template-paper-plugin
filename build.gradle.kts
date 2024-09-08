378f0372f60c3e3239b819829f6b920eff173140
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-09-08 21:20:06 +0100

    Use different shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,7 @@ plugins {
   id("io.papermc.paperweight.userdev") version "1.7.2"
   id("xyz.jpenilla.run-paper") version "2.3.0" // Adds runServer and runMojangMappedServer tasks for testing
   id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.1.1" // Generates plugin.yml based on the Gradle config
-  // Shades and relocates dependencies into our plugin jar. See https://imperceptiblethoughts.com/shadow/introduction/
-  id("io.github.goooler.shadow") version "8.1.7"
+  id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 val groupStringSeparator = "."
