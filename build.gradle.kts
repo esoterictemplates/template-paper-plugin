5aa38eb921a6056bfeebdd3ae8b738f3bde07588
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-09-08 21:19:59 +0100

    Use more constants

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,19 +8,21 @@ plugins {
 }
 
 val groupStringSeparator = "."
+val kebabcaseStringSeparator = "-"
+val snakecaseStringSeparator = "_"
 
 fun capitaliseFirstLetter(string: String): String {
   return string.first().uppercase() + string.slice(IntRange(1, string.length - 1))
 }
 
 fun snakecase(kebabcaseString: String): String {
-  return kebabcaseString.lowercase().replace('-', '_')
+  return kebabcaseString.lowercase().replace(kebabcaseStringSeparator, snakecaseStringSeparator)
 }
 
 fun pascalcase(kebabcaseString: String): String {
   var pascalCaseString = ""
 
-  val splitString = kebabcaseString.split("-")
+  val splitString = kebabcaseString.split(kebabcaseStringSeparator)
 
   for (part in splitString) {
     pascalCaseString += capitaliseFirstLetter(part)
