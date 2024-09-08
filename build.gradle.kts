34f8df0b9bee410b4bc56090b99652ee4439fd86
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-09-08 21:21:06 +0100

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,7 +48,7 @@ val topLevelDomain = "net"
 val projectNameString = rootProject.name
 
 group = topLevelDomain + groupStringSeparator + mainProjectAuthor.lowercase() + groupStringSeparator + snakecase(projectNameString)
-version = "0.0.2"
+version = "0.0.3"
 
 val buildDirectoryString = buildDir.toString()
 
