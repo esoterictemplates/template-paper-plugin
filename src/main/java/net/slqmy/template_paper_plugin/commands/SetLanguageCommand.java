c56dce1d1342c59ca0c0b20f019b234474655681
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-09-08 21:20:20 +0100

    Complete /set-language command

--- src/main/java/net/slqmy/template_paper_plugin/commands/SetLanguageCommand.java
+++ src/main/java/net/slqmy/template_paper_plugin/commands/SetLanguageCommand.java
@@ -1,27 +1,44 @@
 package net.slqmy.template_paper_plugin.commands;
 
 import dev.jorel.commandapi.CommandAPICommand;
-import dev.jorel.commandapi.arguments.MultiLiteralArgument;
+import dev.jorel.commandapi.arguments.ArgumentSuggestions;
+import dev.jorel.commandapi.arguments.GreedyStringArgument;
 
 import java.util.stream.Stream;
 
 import net.slqmy.template_paper_plugin.TemplatePaperPlugin;
+import net.slqmy.template_paper_plugin.data.player.PlayerDataManager;
+import net.slqmy.template_paper_plugin.data.player.PlayerProfile;
 import net.slqmy.template_paper_plugin.language.Language;
 import net.slqmy.template_paper_plugin.language.LanguageData;
+import net.slqmy.template_paper_plugin.language.LanguageManager;
+import net.slqmy.template_paper_plugin.language.Message;
 
 public class SetLanguageCommand extends CommandAPICommand {
 
   public SetLanguageCommand(TemplatePaperPlugin plugin) {
     super("set-language");
 
-    withArguments(new MultiLiteralArgument("language", Stream.of(Language.values()).map((language) -> {
-      LanguageData data = plugin.getLanguageManager().getLanguageData(language);
+    LanguageManager languageManager = plugin.getLanguageManager();
+    PlayerDataManager playerDataManager = plugin.getPlayerDataManager();
+
+    String languageArgumentNodeName = "language";
+
+    withArguments(new GreedyStringArgument(languageArgumentNodeName).includeSuggestions(ArgumentSuggestions.strings(Stream.of(Language.values()).map((language) -> {
+      LanguageData data = languageManager.getLanguageData(language);
 
       return data.getName();
-    }).toArray(String[]::new)));
+    }).toArray(String[]::new))));
 
-    executes((sender, arguments) -> {
+    executesPlayer((player, arguments) -> {
+      String selectedLanguage = (String) arguments.get(languageArgumentNodeName);
+
+      Language language = languageManager.getLanguageByName(selectedLanguage);
       
+      PlayerProfile profile = playerDataManager.getPlayerProfile(player);
+      profile.setLanguage(language);
+
+      player.sendMessage(languageManager.getMessage(Message.SET_LANGUAGE_SUCCESSFULLY, player, selectedLanguage));
     });
 
     register(plugin);
