27d54fa5df804f764b94f4daa1d2679237108f1e
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-09-08 21:21:09 +0100

    Delete example item

--- src/main/java/net/slqmy/template_paper_plugin/custom_item/CustomItemManager.java
+++ src/main/java/net/slqmy/template_paper_plugin/custom_item/CustomItemManager.java
@@ -1,11 +1,9 @@
 package net.slqmy.template_paper_plugin.custom_item;
 
 import net.slqmy.template_paper_plugin.TemplatePaperPlugin;
-import net.slqmy.template_paper_plugin.custom_item.items.SCP018CustomItem;
 
 public class CustomItemManager {
 
   public CustomItemManager(TemplatePaperPlugin plugin) {
-    new SCP018CustomItem(plugin);
   }
 }
--- src/main/java/net/slqmy/template_paper_plugin/language/Message.java
+++ src/main/java/net/slqmy/template_paper_plugin/language/Message.java
@@ -2,7 +2,5 @@ package net.slqmy.template_paper_plugin.language;
 
 public enum Message {
   SET_LANGUAGE_SUCCESSFULLY,
-  UNKNOWN_LANGUAGE,
-
-  SCP018;
+  UNKNOWN_LANGUAGE;
 }
