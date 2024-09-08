2c549ee1f9b142445932321ad0b2165b7d95a4c7
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-09-08 22:59:28 +0100

    Add information if no project name is specified

--- scripts/rename-project.ts
+++ scripts/rename-project.ts
@@ -3,6 +3,9 @@ const commandLineArguments = process.argv.slice(2);
 const projectName = commandLineArguments[0];
 
 if (!projectName) {
-    console.log("A project name must be specified");
+    console.error("A project name must be specified");
+    console.error("Please use the following command:");
+    console.error("npm run rename-project -- new-project-name");
+    console.error("Where 'new-project-name' is replaced with your desired plugin name, in kebab-case format.");
     process.exit(1);
 }
