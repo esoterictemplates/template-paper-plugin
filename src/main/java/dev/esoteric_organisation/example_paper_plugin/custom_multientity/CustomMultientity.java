2ba4a023c53b598a32671cb3dd3ab9ec0c81ae43
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-09-11 22:17:49 +0100

    Revert "Add 'example-paper-plugin/' from commit '0339d21ae0c3ba0e7236f8d996fecad9d1705bbc'"
    This reverts commit 5cc0d67fa47e263f0bd9c2b7cdd782572752bad0, reversing
    changes made to 04893a23f874a379742c01033a3668816663f208.

--- src/main/java/dev/esoteric_organisation/example_paper_plugin/custom_multientity/CustomMultientity.java
+++
@@ -1,9 +0,0 @@
-package dev.esoteric_organisation.example_paper_plugin.custom_multientity;
-
-public enum CustomMultientity {
-  ;
-
-  public static boolean isEnabled() {
-    return values().length != 0;
-  }
-}
