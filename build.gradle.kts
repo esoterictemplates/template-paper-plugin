0e28dfc0d827982a0c1fde09f464cec4793fae10
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-10-11 17:37:40 +0100

    Correct the plugin name

--- build.gradle.kts
+++ build.gradle.kts
@@ -173,7 +173,7 @@ tasks.register("renameProject") {
 }
 
 bukkitPluginYaml {
-  name = providers.gradleProperty("projectName").get()
+  name = pascalcase(providers.gradleProperty("projectName").get())
   description = project.description
   authors = projectAuthors
   setVersion(project.version)
