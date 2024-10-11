8c80f5e324583d63c026017f8e8a0581c884490b
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-10-11 14:21:44 +0100

    Update shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
   id("io.papermc.paperweight.userdev") version "1.7.3"
   id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.2.0"
   id("xyz.jpenilla.run-paper") version "2.3.1"
-  id("io.github.goooler.shadow") version "8.1.7"
+  id("io.github.goooler.shadow") version "8.1.8"
 }
 
 val groupStringSeparator = "."
